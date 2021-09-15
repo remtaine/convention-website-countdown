91d668ce2cf0bb4660cf0f023222716b20f0d867
Benjo Kho <benjokho@gmail.com>
2021-09-16 02:19:59 +0800

    Connect timer per counter

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,15 @@
-const timer = document.getElementById("timer");
+// const timer = document.getElementById("timer");
+// const timerDays = document.getElementById("days");
+// const timerHours = document.getElementById("hours");
+// const timerMinutes = document.getElementById("minutes");
+// const timerSeconds = document.getElementById("seconds");
+
+const timer = {
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    minutes: document.getElementById("minutes"),
+    seconds: document.getElementById("seconds")
+}
 
 // Set the date we're counting down to
 let countDownDate = new Date("Oct 17, 2021 08:00:00").getTime();
@@ -17,13 +28,15 @@ const getCountdownTime = function() {
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    timer.innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    timer.days.innerHTML = days;
+    timer.hours.innerHTML = hours;
+    timer.minutes.innerHTML = minutes;
+    timer.seconds.innerHTML = seconds;
 
     // If the count down is finished, write some text
     if (distance < 0) {
-    clearInterval(x);
-    timer.innerHTML = "EXPIRED";
+        clearInterval(x);
+        // timer.innerHTML = "EXPIRED";
     }
 }
 
